7e667dc9e75158bb77c26b2d3db6121ce1f2ca2d
Kevin Augustino <kevinaugustino@hotmail.com>
2022-05-10 20:11:41 -0400

    Add 'Rice' to Food Bags.

--- ItemBags/Persistence/BagTypeFactory.cs
+++ ItemBags/Persistence/BagTypeFactory.cs
@@ -2320,7 +2320,7 @@ namespace ItemBags.Persistence
             List<int> HasQualitiesIds = new List<int>() { 346, 459, 303 }; // Beer, Mead, Pale Ale
 
             List<int> Ingredients = new List<int>() {
-                246, 245, 247, 419, 724, 814, 921 // Wheat Flour, Sugar, Oil, Vinegar, Rice, Maple Syrup, Squid Ink, Squid Ink Ravioli
+                246, 245, 247, 419, 423, 724, 814, 921 // Wheat Flour, Sugar, Oil, Vinegar, Rice, Maple Syrup, Squid Ink, Squid Ink Ravioli
             };
 
             List<int> CraftedFoods = new List<int>() {
@@ -2353,11 +2353,6 @@ namespace ItemBags.Persistence
 
             List<int> MiscFoods = new List<int>() {
                 78 // Cave Carrot
-            };
-
-            BagType bt = new BagType
-            {
-                Description = ""
             };
 
             return new BagType()
