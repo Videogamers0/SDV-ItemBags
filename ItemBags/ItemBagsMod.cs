9e4ea6fe44ca8df69d9e9c5325d5d3c9b229c0ce
Kevin Augustino <kevinaugustino@hotmail.com>
2024-04-02 04:20:30 -0400

    Update version

--- ItemBags/ItemBagsMod.cs
+++ ItemBags/ItemBagsMod.cs
@@ -29,7 +29,7 @@ namespace ItemBags
         internal static LogLevel InfoLogLevel = LogLevel.Trace;
 #endif
 
-        public static Version CurrentVersion = new Version(2, 0, 3); // Last updated 12/4/2021 (Don't forget to update manifest.json)
+        public static Version CurrentVersion = new Version(3, 0, 5); // Last updated 4/1/2024 (Don't forget to update manifest.json)
         public const string ModUniqueId = "SlayerDharok.Item_Bags";
         public const string JAUniqueId = "spacechase0.JsonAssets";
         public const string SpaceCoreUniqueId = "spacechase0.SpaceCore";
